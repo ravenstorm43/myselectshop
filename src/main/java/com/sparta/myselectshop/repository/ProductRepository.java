02979a797082aa742a9bb6d7f55e7149b11b7747
ravenstorm43 <201912065@office.induk.ac.kr>
2024-05-29 15:41:34 +0900

    상품 폴더 설계

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findAllByUser(User user, Pageable pageable);
+    Page<Product> findAllByUser(User user);
 }
